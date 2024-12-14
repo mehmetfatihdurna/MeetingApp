7b63f15fe2498c72df71bcdb3f0a0b2abc1219e2
Mehmet Fatih Durna <mfd304603@gmail.com>
2024-12-14 17:52:23 +0300

    ViewBag, ViewData, Layout, @Model, TagHelpers, _ViewImport, _ViewStart

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,24 @@
+using MeetingApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MeetingApp.Controllers{
     public class HomeController: Controller{
         public IActionResult Index(){
-            return View();
+            int hour = DateTime.Now.Hour;
+            ViewBag.Selamlama = hour > 12 ? "İyi Günler" : "Günaydın";
+
+            ViewBag.UserName = "Fatih";
+
+            ViewData["saat"] = DateTime.Now.Hour;
+            ViewData["dakika"] = DateTime.Now.Minute;
+
+            MeetingInfo meeting = new(){
+                Id=1,
+                Date=new DateTime(2024,12,14,20,0,0),
+                Location="Eskisehir",
+                NumberOfPeople=1};
+
+            return View(meeting);
         }
     }
 }
---
+++ Models/MeetingInfo.cs
@@ -0,0 +1,13 @@
+namespace MeetingApp.Models{
+    public class MeetingInfo{
+        public string? Location{get;set;}
+
+        public int Id {get;set;}
+
+        public DateTime Date{get;set;}
+
+        public int NumberOfPeople{get;set;}
+
+
+    }
+}
